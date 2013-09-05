b1601b3081581c055ad6829062cd19d16b5256c6
m.kerkez <m.kerkez@INTERNSHIP7.levi9.com>
2013-09-05 10:25:45 +0200

    Movie class in model

---
+++ videostore/src/main/java/rs/code9/videostore/model/Movie.java
@@ -0,0 +1,57 @@
+package rs.code9.videostore.model;
+
+public class Movie {
+
+	private String name;
+	
+	private String genre;
+	
+	private String descripiton;
+	
+	private int year;
+	
+	private int counter;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getDescripiton() {
+		return descripiton;
+	}
+
+	public void setDescripiton(String descripiton) {
+		this.descripiton = descripiton;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+	
+	
+	
+}
